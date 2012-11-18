34169820a4ea46f83b1e19591fe01a6b34663b8f
astrolitterbox <sbekeraite@aip.de>
2012-11-18 16:30:02 +0100

    exponentials

--- gc.py
+++ gc.py
@@ -33,19 +33,19 @@ mu5 = 3
 sigma5 = 1
 
 def funct(x):
-	ret = 22*np.exp(x * -0.2)
+	ret = np.exp(x * -0.2)
 	print ret
 	return ret
 
-fig = plt.figure(figsize = (16, 12))
+fig = plt.figure(figsize = (12, 12))
 l = np.linspace(0, 30, size)
 
 #flux profile
 
-func = 22*np.exp(l * -0.2)
+flux = funct(l)/np.max(funct(l))
 
 ax1 = fig.add_subplot(311)
-ax1.plot(l, funct(l), 'b', lw=1)
+ax1.plot(l, flux, 'b', lw=1)
 ax1.set_axis_off()
 #ax1.text(mu1-2, -3, "Body hair")
 #ax1.text(mu2-4, -3, "Scalp hair")
@@ -56,7 +56,7 @@ ax1.set_title('Exponential flux profile')
 ax1.set_xlabel('distance from the center')
 ax1.set_ylabel('Counts')
 #ax1.set_xlim(0, 26)
-ax1.set_ylim(0, 20)
+ax1.set_ylim(0, 1)
 
 #xkcdify
 XKCDify(ax1, xaxis_loc=0.0, yaxis_loc=0.0,
@@ -64,16 +64,9 @@ XKCDify(ax1, xaxis_loc=0.0, yaxis_loc=0.0,
         expand_axes=False)
 
 ax2 = fig.add_subplot(312)
-def cumf(l):
-	return scipy.integrate.quad(lambda x: funct(x), l, l+1)
-
-cf = np.empty((l.shape))
-for i, x in enumerate(l):
-	print i, 'i'
-	cf[i] = cumf(x)[0]
-	
-print cf		
-ax2.plot(l, cf, 'b', lw=1)
+		
+		
+ax2.plot(l, np.cumsum(flux)/np.max(np.cumsum(flux)), 'b', lw=1)
 ax2.set_axis_off()
 #ax1.text(mu1-2, -3, "Body hair")
 #ax1.text(mu2-4, -3, "Scalp hair")
@@ -81,13 +74,13 @@ ax2.set_axis_off()
 ax2.set_title('Cumulative flux profile')
 #ax1.text(mu1+1, 16, "People with uncut hair")
 
-ax2.set_xlabel('distance from the center')
-ax2.set_ylabel('Total counts inside the given ellipse')
-ax2.set_ylim(0, 20)
+ax2.set_xlabel('Distance from the center')
+ax2.set_ylabel('Cumulative counts')
+ax2.set_ylim(0, 1)
 
 #xkcdify
 XKCDify(ax2, xaxis_loc=0.0, yaxis_loc=0.0,
         xaxis_arrow='+-', yaxis_arrow='+-',
         expand_axes=False)
 
-plt.savefig('flux_profile')
+plt.savefig('flux_profile', bbox_inches='tight')
